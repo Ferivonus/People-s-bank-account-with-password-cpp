f391f51e4a9448b7c6205e6f1725a4ef9b40ff77
Fahrettin Baştürk <ferivonus@hotmail.com>
2021-11-05 15:08:10 +0300

    Update BankAccount _with_password.cpp

--- BankAccount _with_password.cpp
+++ BankAccount _with_password.cpp
@@ -1,6 +1,7 @@
 // 
-// BankAccount with password 
-// Codded by Fahrettin Baştürk
+// codded by Fahrettin Baştürk.
+// BankAccount with password
+// version: 2.0.0
 //
 
 #include <iostream>
@@ -8,16 +9,34 @@
 using namespace std;
 #include "BankClass.h"
 #include "PeopleClass.h"
+int PeopleCounterFunc();
+
 
 int main()
 {
-    string name = "fahrettin", surname = "basturk";
-    int money = 5000, age = 22;
-    People Fahrettin(name, surname, money, age);
-    if (Fahrettin.BankAccountCheck())
+    string name = "", surname = "";
+    int money = 0, age = 0, HowManyPeople;
+    HowManyPeople = PeopleCounterFunc();
+    People* MyPeople[5] = { 0 };
+    for (int i = 0; i < HowManyPeople; i++)
     {
-        Fahrettin.Manage_BankAccount(money);
+        i++;
+        cout << "Type " << i << " Person's name and surname: ";
+        i--;
+        cin >> name >> surname;
+        cout << "type " << name + " " + surname << "'s money and age: ";
+        cin >> money >> age;
+        MyPeople[i] = new People(name, surname, money, age);
     }
     return 0;
+}
+
+
+int PeopleCounterFunc()
+{
+    int returner;
+    cout << "How many people do we have: ";
+    cin >> returner;
+    return returner;
 }
 
